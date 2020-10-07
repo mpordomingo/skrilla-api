63fac82fa15d98992b533aa06f166baf6ad4de17
Martin Pordomingo <martinpordomingo@gmail.com>
2020-10-07 19:55:10 -0300

    build fix

---
+++ skrilla-api/Services/ConsumptionService.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.Extensions.Logging;
+using skrilla_api.Configuration;
+using skrilla_api.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using NodaTime;
+using Microsoft.EntityFrameworkCore;
+
+namespace skrilla_api.Services
+{
+    public class ConsumptionService : IConsumptionService
+    {
+        private readonly MysqlContext dbContext;
+
+        private readonly ILogger<ConsumptionService> _logger;
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ConsumptionService(ILogger<ConsumptionService> logger,
+            MysqlContext context, IHttpContextAccessor httpContextAccesor)
+        {
+            _logger = logger;
+            _httpContextAccessor = httpContextAccesor;
+            dbContext = context;
+        }
+
+        public bool ModifyConsumption(ConsumptionRequest consumptionRequest, int id)
+        {
+            string loggedUser = _httpContextAccessor.HttpContext.User.FindFirstValue("userId");
+
+            Consumption consumption = dbContext
+                   .Consumptions
+                   .Where(s => s.Id == id && s.PersonId.Equals(loggedUser))
+                   .Include(c => c.Category).FirstOrDefault();
+
+            if (consumption == null)
+            {
+                _logger.LogDebug("Consumption not found: " + id + " for user: " + loggedUser);
+                throw new SkrillaApiException("not_found", "Consumption not found");
+            }
+
+            UpdateValues(consumption, consumptionRequest);
+            dbContext.SaveChanges();
+            return true;
+        }
+
+        public bool DeleteConsumption(int id)
+        {
+            string loggedUser = _httpContextAccessor.HttpContext.User.FindFirstValue("userId");
+
+            Consumption consumption = dbContext
+                    .Consumptions
+                    .Where(s => s.Id == id && s.PersonId.Equals(loggedUser))
+                    .Include(c => c.Category).FirstOrDefault();
+
+            if (consumption == null)
+            {
+                _logger.LogDebug("Consumption not found: " + id + " for user: " + loggedUser);
+                throw new SkrillaApiException("not_found","Consumption not found");
+            }
+
+            dbContext.Remove<Consumption>(consumption);
+            dbContext.SaveChanges();
+
+            return true;
+        }
+
+        public Consumption CreateConsumption(ConsumptionRequest request)
+        {
+            string loggedUser = _httpContextAccessor.HttpContext.User.FindFirstValue("userId");
+            Category category = GetOrCreateCategory(request.Category);
+
+            Consumption consumption = new Consumption(request.Title,
+                request.Amount,
+                category,
+                loggedUser,
+                LocalDate.FromDateTime(request.Date));
+
+            dbContext.Add(consumption);
+            dbContext.SaveChanges();
+            return consumption;
+        }
+
+        public List<Consumption> GetConsumptions(string category)
+        {
+            string loggedUser = _httpContextAccessor.HttpContext.User.FindFirstValue("userId");
+
+            if (category != null)
+            {
+                List<Consumption> result = new List<Consumption>();
+                result = dbContext
+                    .Consumptions
+                    .Where(s => s.Category.Name.Contains(category) && s.PersonId.Equals(loggedUser))
+                    .Include(c => c.Category).ToList();
+
+                return result;
+            }
+            else
+            {
+                return dbContext.Consumptions
+                    .Where(s => s.PersonId.Equals(loggedUser))
+                    .Include(c => c.Category)
+                    .ToList();
+            }
+        }
+
+        private void UpdateValues(Consumption consumption, ConsumptionRequest request)
+        {
+            consumption.Title = request.Title;
+            consumption.Amount = request.Amount;
+            consumption.Category = GetOrCreateCategory(request.Category);
+            consumption.Date =  LocalDate.FromDateTime(request.Date);
+
+        }
+
+        private Category GetOrCreateCategory(string category)
+        {
+            string loggedUser = _httpContextAccessor.HttpContext.User.FindFirstValue("userId");
+
+            List<Category> categories = dbContext
+                .Categories
+                .Where(s => s.Name == category)
+                .ToList<Category>();
+
+            Category aCategory;
+            if (categories.Count == 0)
+            {
+
+                aCategory = new Category(
+                    category,
+                    true,
+                    loggedUser);
+
+                dbContext.Add(aCategory);
+                dbContext.SaveChanges();
+            }
+            else
+            {
+                aCategory = categories.First<Category>();
+            }
+
+
+            return aCategory;
+        }
+    }
+}
--- skrilla-api/Startup.cs
+++ skrilla-api/Startup.cs
@@ -4,26 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using skrilla_api.Configuration;
 using skrilla_api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
-using IdentityServer4.Services;
 using skrilla_api.Services;
 
 namespace skrilla_api
 {
     public class Startup
     {
-        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -31,7 +25,6 @@ namespace skrilla_api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(options =>
@@ -69,11 +62,8 @@ namespace skrilla_api
 
             services.AddHttpContextAccessor();
             services.AddScoped<IConsumptionService, ConsumptionService>();
-
-
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
