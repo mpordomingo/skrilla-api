6925f670e5b94d21dd084d2f0bc7df2469a88ac2
Martin Pordomingo <martinpordomingo@gmail.com>
2020-10-08 16:26:15 -0300

    more tests

--- SkrillaApi.Tests/Tests/ConsumptionControllerTests.cs
+++ SkrillaApi.Tests/Tests/ConsumptionControllerTests.cs
@@ -29,6 +29,14 @@ namespace SkrillaApi.Tests.Tests
                 .Setup(service => service.GetConsumptions(It.IsAny<string>()))
                 .Returns((string cate) => consumptions.FindAll(con => con.Category.Name.Contains(cate)));
 
+            consumptionServiceMock
+                .Setup(service => service.CreateConsumption(It.IsAny<ConsumptionRequest>()))
+                .Returns(consumptions.Find(c => "Example1".Equals(c.Title)));
+
+            consumptionServiceMock
+                .Setup(service => service.DeleteConsumption(It.IsAny<int>()))
+                .Returns(true);
+
             controller = new ConsumptionsController(loggerMock.Object, consumptionServiceMock.Object);
 
             ClaimsPrincipal principal = new ClaimsPrincipal();
@@ -58,15 +66,50 @@ namespace SkrillaApi.Tests.Tests
             
         }
 
+        [Fact]
+        public void CreateConsumptionReturnsCreatedConsumption()
+        {
+            ConsumptionRequest request = new ConsumptionRequest
+            {
+                Title = "Example1",
+                Amount = 345.6,
+                Category = "ExampleCategory",
+                Date = new DateTime(2020, 04, 15)
+            };
+
+
+            var result = controller.Post(request);
+            Assert.NotNull(result);
+            var resultObject = result.Result as ObjectResult;
+            var createdConsumption = (Consumption)resultObject.Value;
+
+            Assert.IsType<CreatedAtActionResult>(resultObject);
+            Assert.Equal("Example1", createdConsumption.Title);
+            Assert.Equal(345.6, createdConsumption.Amount);
+            Assert.Equal(2020, createdConsumption.Date.Year);
+            Assert.Equal(14, createdConsumption.Date.Day);
+            Assert.Equal(4, createdConsumption.Date.Month);
+        }
+
+        [Fact]
+        public void ConsumptionDeletionIsSuccessful()
+        {
+            var result = controller.DeleteConsumption(12);
+            Assert.NotNull(result);
+            var resultObject = result as ObjectResult;
+
+            Assert.IsType<AcceptedResult>(resultObject);
+        }
+
         private List<Consumption> InitializeConsumptionsList()
         {
             List<Consumption> consumptions = new List<Consumption>();
 
             Category category1 = new Category("ExampleCategory", false, "mockUser");
             Category category2 = new Category("ExampleCategory2", false, "mockUser");
-            Consumption consumption1 = new Consumption("Example", 345.6, category1,
+            Consumption consumption1 = new Consumption("Example1", 345.6, category1,
                 "mockUser", new LocalDate(2020, 04, 14));
-            Consumption consumption2 = new Consumption("Example", 345.6, category2,
+            Consumption consumption2 = new Consumption("Example2", 345.6, category2,
                 "mockUser", new LocalDate(2020, 04, 15));
 
             consumptions.Add(consumption1);
