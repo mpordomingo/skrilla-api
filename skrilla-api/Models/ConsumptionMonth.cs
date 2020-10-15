050e7b317b2b5d61dce401f85e52fb655f2e0962
tomasnozica <tomas.nozica@gmail.com>
2020-10-15 16:59:31 -0300

    cons month

---
+++ skrilla-api/Models/ConsumptionMonth.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using NodaTime;
+namespace skrilla_api.Models
+{
+    public class ConsumptionMonth
+    {
+        [Required]
+        public int Month { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public double Amount { get; set; }
+
+        public ConsumptionMonth(int month,int year,double amount)
+        {
+            this.Month = month;
+            this.Year = year;
+            this.Amount = amount;
+        }
+    }
+}
